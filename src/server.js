79794e3ba74f11526c997c633e1ada09ccd9b165
blessdyb <blessdyb@gmail.com>
2024-12-01 19:53:47 -0800

    introduce db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require("express");
 const next = require("next");
+const mysql = require("mysql2");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -8,10 +9,30 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+  db.connect((err) => {
+    if (err) {
+      console.error("Database connection failed:", err.stack);
+      return;
+    }
+    console.log("Connected to MySQL database.");
+  });
 
   // Add your Express.js API routes here
   server.get("/nextjs-helloworld/api/data", (req, res) => {
-    res.json({ message: "Hello from Express.js! " });
+    const query = "SELECT country, code_section FROM data";
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err.stack);
+        return res.status(500).send("Error executing query");
+      }
+      res.json(results);
+    });
   });
 
   // Let Next.js handle all other routes
