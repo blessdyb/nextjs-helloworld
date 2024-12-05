c64619616e15f81135826c941b744db587621c21
blessdyb <blessdyb@gmail.com>
2024-12-04 19:13:02 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>TEST AFTER BUILD</h1>
+        <h1>TEST AFTER BUILD ~ Hello</h1>
       </main>
     </div>
   );
